dba4313115a0d421d26bf229b4924b4487daa094
dishasnayak <86235846+dishasnayak@users.noreply.github.com>
2021-09-06 17:12:36 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# plinko-debuging
-completed
+
---
+++ sketch.js
@@ -0,0 +1,84 @@
+//var ,no const here
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+// not needed const  Events = Matter.Events
+
+var ground
+var world,engine
+var particles = [];
+var plinkos = [];
+//not there
+var divisions =[];
+var divisionHeight=300;
+var score =0;
+function preload(){
+
+}
+
+
+function setup() {
+  createCanvas(800, 700);
+  engine = Engine.create();
+  world = engine.world;
+  
+  //create body
+ ground = new Ground (400,690,800,20);
+
+
+  for (var i = 0; i<=width; i= i+80) {
+    divisions.push(new Division(i,height-divisionHeight/2, 10, divisionHeight));
+}
+
+for (var j=75; j<=width; j = j+50) {
+  plinkos.push(new Plinko(j, 75));
+}
+for (var j=50; j<=width-10; j=j+50) {
+  plinkos.push(new Plinko(j,175));
+}
+for (var j=75; j<=width; j=j+50) {
+  plinkos.push(new Plinko(j,275));
+}
+for (var j=50; j<=width-10; j=j+50) {
+  plinkos.push(new Plinko(j,375));
+}
+
+
+
+
+Engine.run(engine);
+
+}
+
+
+function draw() {
+rectMode(CENTER);
+background(0);
+
+drawSprites();
+
+
+
+for (var n = 0; n<divisions.length; n++) {
+  divisions[n].display();
+}
+
+if (frameCount %60 === 0) {
+  particles.push(new Particle(random(width/2-30, width/2+30),10,10));
+}
+
+for (var h = 0; h<particles.length; h++) {
+  particles[h].display();
+}
+
+for (var j = 0; j<plinkos.length; j++) {
+  plinkos[j].display();
+}
+
+
+ground.display();
+
+
+
+}
